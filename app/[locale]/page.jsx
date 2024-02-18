fc8510c4e8e142587ccc00a7d06dac5c432c5b4a
houssein sedoum <nejihoussein1@gmail.com>
2024-02-18 23:42:35 +0000

    remove projectSEction

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -2,7 +2,7 @@ import HeroSection from "./components/HeroSection/HeroSection"
 import HeaderSection from "./components/Layout/headerSection/HeaderSection"
 import AboutSection from "./components/aboutSection/AboutSection"
 import ContactSection from "./components/ContactSection/ContactSection"
-import ProjectSection from "./components/projectSection/ProjectSection"
+// import ProjectSection from "./components/projectSection/ProjectSection"
 const Home = () => {
   return (
     <main>
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="mx-auto mt-0 sm:mt-10 px-5 md:px-12">
         <HeroSection />
         <AboutSection />
-        <ProjectSection />
+        {/* <ProjectSection /> */}
         <ContactSection />
       </div>
     </main>
